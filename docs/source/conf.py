b411d9620bf45c1e852d2f5474749abf73f2b435
WampyCakes <WampyCakes@users.noreply.github.com>
2021-05-03 20:53:29 -0400

    idk

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,10 +14,10 @@ import os
 import sys
 from distutils.sysconfig import get_python_lib
 
-sys.path.insert(0, os.path.abspath('.'))
-
 import manim
-# import manim_rubikscube
+import manim_rubikscube
+
+sys.path.insert(0, os.path.abspath('.'))
 
 if os.environ.get("READTHEDOCS") == "True":
     site_path = get_python_lib()
